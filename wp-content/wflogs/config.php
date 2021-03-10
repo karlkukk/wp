c35b554d01a73e4ea1c54bb9697c7098d68d0635
karlkukkiktkhk <karl.kukk@khk.ee>
2021-03-10 12:26:52 +0200

    modified file `config.php` (after activation of `Google Tag Manager for Wordpress` version 1.11.6)

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614174820;s:7:"authKey";s:64:"VP2I8=OV>a_~rSK64VTiLW77*CEKXwmLLZok85gT~=:9W9QfwO^@wtPf:a##lEx~";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3232;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"VP2I8=OV>a_~rSK64VTiLW77*CEKXwmLLZok85gT~=:9W9QfwO^@wtPf:a##lEx~";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3232;}
